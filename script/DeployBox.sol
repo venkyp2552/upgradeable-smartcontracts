6fff13424f1e35ed939ecef0cdee90414339367e
root <root@DESKTOP-1HVSU4I>
2025-01-30 19:39:03 +0530

    UpgradeBx

--- script/DeployBox.sol
+++ script/DeployBox.sol
@@ -12,8 +12,10 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
     }
 
     function deployBox() public returns(address){
+        vm.startBroadcast();
         BoxV1 box=new BoxV1(); // Implementation Contract (Logic contract)
         ERC1967Proxy proxy=new ERC1967Proxy(address(box),"");
+        vm.stopBroadcast();
         return address(proxy);
     }
 }
--- script/UpgradeBox.sol
+++ script/UpgradeBox.sol
@@ -1,2 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from"lib/foundry-devops/src/DevOpsTools.sol";
+import {BoxV2} from "../src/BoxV2.sol";
+import {BoxV1} from "../src/BoxV1.sol";
+
+ contract UpgaradeBox is Script{
+
+    function run() external returns(address){
+        address mostRecentDeployed=DevOpsTools.get_most_recent_deployment("ERC1967Proxy",block.chainid);
+
+        vm.startBroadcast();
+        BoxV2 newbox=new BoxV2();
+        vm.stopBroadcast();
+        address proxy=upgradeBox(mostRecentDeployed,address(newbox));
+    }
+
+    function upgradeBox(address proxyAddress,address newBox) public returns(address){
+        vm.startBroadcast();
+        BoxV1 proxy=BoxV1(proxyAddress);
+        proxy.upgradeTo(address(newBox));
+        vm.stopBroadcast();
+        return address(newBox);
+    }
+
+}
